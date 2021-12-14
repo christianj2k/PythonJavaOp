205762c55ed86abe154491bc95008e2e83a830ef
Semiotic <78098764+christianj2k@users.noreply.github.com>
2021-12-14 09:53:28 -0500

    Password Generator V1

---
+++ Password_Gen.py
@@ -0,0 +1,28 @@
+# Random Password Generator
+
+import random
+import sys
+
+def passV1(x):
+    password = ""
+    value = int(x)
+    for i in range(value // 2):
+        a = chr(random.randint(65,90))
+        b = chr(random.randint(65,90))
+        c = chr(random.randint(60,71))
+        d = chr(random.randint(41,57))
+        e = chr(random.randint(65,90)).lower()
+        up_low = a + e
+        shuffle  =  b + c + d + e
+        password = str(password)+ ''.join(random.sample(up_low,len(up_low))) + ''.join(random.sample(shuffle,len(shuffle)))
+    if(len(password)>value):
+        extra = len(password) - value
+        password = password[0:len(password)-extra]
+    print("Password: " + password)
+
+if(len(sys.argv) == 1):
+    user = input("Enter even number of characters for password:\n")
+    passV1(user)
+else:
+    user = sys.argv[1]
+    passV1(user)
